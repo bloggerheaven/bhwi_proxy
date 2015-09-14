7ca67966a48e8d8b2a7c94151c82fb0857d04fd9
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-09-14 18:32:20 +0200

    hello world with param

--- bhwi_proxy.py
+++ bhwi_proxy.py
@@ -4,9 +4,9 @@ import os
 app = Flask(__name__)
 app.debug = os.getenv('DEBUG', '') == 'True'
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/recent_images/<path:name>")
+def recent_images(name):
+    return 'Hello ' + name
 
 if __name__ == "__main__":
     app.run()
