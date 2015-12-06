4b8756096a7e60df361eac03b8325d0a4f58176e
Cyril Kyburz <cyril.kyburz@renuo.ch>
2015-12-06 16:29:15 +0100

    keep it simple, fix cors

--- bhwi_proxy.py
+++ bhwi_proxy.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify, abort
-from flask.ext.cors import CORS, cross_origin
 import os
 import requests
 
 app = Flask(__name__)
 app.debug = os.getenv('DEBUG', '') == 'True'
-CORS(app, resources=r'/recent_images/*', allow_headers='Content-Type')
 
 def build_url(user_id, client_id):
   return 'https://api.instagram.com/v1/users/' + user_id + '/media/recent/?client_id=' + client_id + '&callback=?'
 
 @app.route("/recent_images/<path:user_id>")
-@cross_origin()
 def recent_images(user_id):
     if user_id in os.getenv('USER_IDS', ''):
       path = build_url(user_id, os.getenv('CLIENT_ID', ''))
       r = requests.get(path)
-      return jsonify(r.json(), status=202)
+      response = jsonify(r.json(), status=202)
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      return response
     else:
       abort(403)
 
